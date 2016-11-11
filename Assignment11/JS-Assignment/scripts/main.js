1d230c78d9088c4993ab9467415c59806f361c23
jdelaplane <student18@ITR410Mac18.local>
2016-11-10 20:40:00 -0600

    assignment 11

---
+++ Assignment11/JS-Assignment/scripts/main.js
@@ -0,0 +1,45 @@
+var messages = [];
+  var firstName = document.getElementById('firstName').value;
+  var lastName = document.getElementById('lastName').value;
+  var emailAddress = document.getElementById('emailAddress').value;
+  var html = '<ul>';
+
+
+  if ( firstName === '' ) {
+      messages.push('First name is required');
+  }
+
+  if ( lastName === '' ) {
+      messages.push('Last name is required');
+  }
+
+  if ( emailAddress === '' ) {
+      messages.push('Email is required');
+  }
+
+  var user {
+
+    firstName: "firstname"
+    lastName: "lastname"
+    emailAddress: "email address"
+
+    fullname: function () { firstName + '' lastName
+
+    }
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////
