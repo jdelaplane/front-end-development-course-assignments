718fd4e23c6be7c83429a98e50a18f2d224e0fbe
jdelaplane <jdelaplane@live.com>
2016-11-18 12:52:01 -0600

    assignment 12

---
+++ Assignment12/scripts/main.js
@@ -0,0 +1,21 @@
+
+
+var button = getElementById('btnSubmit');button.addEventListener('click', submit);
+
+var button2 = getElementById('btnReset');button2.addEventListener('click', reset);
+
+var inputs = ['firstName', 'lastName', 'emailAddress', 'password', 'passwordConfirm']
+
+
+
+
+function submit {
+
+  if (firstName === "") {
+
+    messages.push(First Name is Required)
+
+  }
+
+
+}
