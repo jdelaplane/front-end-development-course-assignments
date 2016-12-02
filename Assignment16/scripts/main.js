20b93b951002ae699147c66a16ff70fc4829ee4a
jdelaplane <student18@ITR410Mac18.local>
2016-12-01 19:22:42 -0600

    Assignment16

--- Assignment16/scripts/main.js
+++ Assignment16/scripts/main.js
@@ -1,5 +1,84 @@
 $(document).ready(function(){
 
-
-
 });
+
+
+$('#btnShow').on('click', function () {
+
+    $('.display-basic').show();
+
+})
+
+$('#btnHide').on('click', function () {
+
+    $('.display-basic').hide();
+
+})
+
+
+$('#btnToggle').on('click', function () {
+
+    $('.display-basic').toggle();
+
+})
+
+
+
+/* function pageLoad() {
+
+        $('#part3').show();
+        $('#part3').show();
+
+      } */
+
+      //$(document).on("pageload",function(){
+        $('.part3').show();
+    //  });
+
+
+/// Part 4
+
+
+$('#btnFadeIn').on('click', function () {
+
+    $('.isplay-fade').fadeIn(5000);
+
+})
+
+$('#btnFadeOut').on('click', function () {
+
+    $('.isplay-fade').fadeOut(5000);
+
+})
+
+
+$('#btnFadeToggle').on('click', function () {
+
+    $('.isplay-fade').fadeToggle(5000);
+
+})
+
+
+/// Part 5
+
+
+$('#btnSlideUp').on('click', function () {
+
+    $('.display-slide').slideUp(5000);
+
+})
+
+
+$('#btnSlideDown').on('click', function () {
+
+    $('.display-slide').slideDown(5000);
+
+})
+
+
+
+$('#btnSlideToggle').on('click', function () {
+
+    $('.display-slide').slideToggle();
+
+})
