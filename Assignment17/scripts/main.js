841b89ca2713066e30abe0a2b4cbe4eabe07068a
jdelaplane <student18@ITR410Mac18.local>
2016-12-08 19:32:31 -0600

    Assignment17

--- Assignment17/scripts/main.js
+++ Assignment17/scripts/main.js
@@ -3,3 +3,19 @@ $(document).ready(function(){
 
 
 });
+
+$('.error').hide();
+
+/*$('.input-groups').filter(':even').addClass('even');
+
+$('.input-group input').on('blur', function (){
+
+  var inputs = $(this);
+
+  if (inputs.val() ====)
+
+
+
+
+
+});
