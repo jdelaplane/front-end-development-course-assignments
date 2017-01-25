9fa325c47b8e65a38b08716329765c5313665f55
jdelaplane <student18@ITR410Mac18.local>
2017-01-24 19:10:01 -0600

    Assignment26

---
+++ Assignment26/basic/scripts/main.js
@@ -0,0 +1,19 @@
+$('#btnShowModal').on('click', showDialog);
+
+function showDialog() {
+  $('.overlay').fadeIn();
+  $('.modal').slideDown();
+
+  $('#btnClose').on('click', hideDialog);
+  $('.overlay').on('click', hideDialog);
+
+}
+
+function hideDialog() {
+  $('.overlay').fadeOut();
+  $('.modal').slideUp();
+
+  $('#btnClose').off('click', hideDialog);
+  $('.overlay').off('click', hideDialog);
+
+}
